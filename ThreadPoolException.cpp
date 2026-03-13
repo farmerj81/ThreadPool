da170d8c919ffee9a34543bdb61f1e2e82647b2d
Jim Farmer <pvorhagen@gmail.com>
2026-03-13 08:28:30 +0100

    Update ThreadPoolException.cpp

--- ThreadPoolException.cpp
+++ ThreadPoolException.cpp
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2011 Pierre Vorhagen, Etienne Doms.
+ * Copyright 2011
  *
  * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
